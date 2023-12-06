069815264b40b850a48b0331d7273c9fb4856e5a
lolearningcode <lolearningcode@gmail.com>
2023-12-06 13:12:56 -0800

    Request data on load

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,19 +8,37 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         let _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        //Given
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+
+        // When
+        sut.load()
+        
+        // Then
+        XCTAssertNotNil(client.requestedURL)
     }
 }
