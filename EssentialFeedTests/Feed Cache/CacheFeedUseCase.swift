935a8351f3bacaf823446ad132c41e8b93437bea
lolearningcode <lolearningcode@gmail.com>
2023-12-12 05:52:07 -0800

    Extract sut creation to a factory method to protect against tests breaking changes

--- EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -31,15 +31,13 @@ class FeedStore {
 class CacheFeedUseCase: XCTestCase {
     
     func test_init_doesNotDeleteCacheUponCreation() {
-        let store = FeedStore()
-        let _ = LocalFeedLoader(store: store)
+        let (_, store) = makeSUT()
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
     func test_save_requestCacheDeletion() {
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        let (sut, store) = makeSUT()
         let items = [uniqueItems(), uniqueItems()]
         
         sut.save(items)
@@ -49,6 +47,14 @@ class CacheFeedUseCase: XCTestCase {
     }
     
     // MARK: - HELPERS
+    
+    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        return (sut, store)
+    }
+    
     private func uniqueItems() -> FeedItem {
         return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
     }
