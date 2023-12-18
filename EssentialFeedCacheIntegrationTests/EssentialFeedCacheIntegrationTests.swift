8f8479d9e542ea0ad2a2ce2babe402fd40b07640
lolearningcode <lolearningcode@gmail.com>
2023-12-18 13:02:45 -0800

    Created 'EssentialFeedCacheIntegrationTests' test target

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Cleo Howard on 12/18/23.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
