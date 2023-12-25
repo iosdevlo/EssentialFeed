4744fbc0cd61f6b4f4e2fe8ab1bdc3653b500e78
lolearningcode <lolearningcode@gmail.com>
2023-12-25 14:37:55 -0800

    Extract 'cancelImageDataLoad(from: URL)'
    from 'FeedImageDataLoader' protocol into a new 'FeedImageDataLoaderTask' protocol that represents a task that can be cancelled. This way, we respect the Interface segregation principle and 'FeedImageDataLoader' implementations are not forced to be stateful

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -9,14 +9,18 @@ import UIKit
 import EssentialFeed
 
 public protocol FeedImageDataLoader {
-    func loadImageData(from url: URL)
-    func cancelImageDataLoad(from url: URL)
+    func loadImageData(from url: URL) -> FeedImageDataLoaderTask
+}
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
 }
 
 final public class FeedViewController: UITableViewController {
     
     private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
+    private var tasks = [IndexPath: FeedImageDataLoaderTask]()
     
     private var tableModel = [FeedImage]()
     
@@ -61,13 +65,13 @@ final public class FeedViewController: UITableViewController {
         cell.locationLabel.text = cellModel.location
         cell.descriptionLabel.text = cellModel.description
         
-        imageLoader?.loadImageData(from: cellModel.url)
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url)
         
         return cell
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let cellModel = tableModel[indexPath.row]
-        imageLoader?.cancelImageDataLoad(from: cellModel.url)
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
     }
 }
