ea73b9fb684e572b1a90c952b98547649665c103
lolearningcode <lolearningcode@gmail.com>
2024-01-15 05:00:29 -0800

    FeedImageDataCacheDecorator implementation

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -36,9 +36,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)
         
         let feedViewController = FeedUIComposer.feedComposedWith(
-            feedLoader: FeedLoaderWithFallbackComposite(primary: remoteFeedLoader,
-                                                        fallback: FeedLoaderWithFallbackComposite(primary: remoteFeedLoader,
-                                                                                                  fallback: localFeedLoader)),
+            feedLoader: FeedLoaderWithFallbackComposite(primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader,
+                                                                                          cache: localFeedLoader),
+                                                        fallback: localFeedLoader),
                                                                  
             imageLoader: FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader,
                                                                   fallback: remoteImageLoader))
