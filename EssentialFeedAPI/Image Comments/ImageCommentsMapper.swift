402c670ce1c6766b516ed94bca3db84d506451fc
lolearningcode <lolearningcode@gmail.com>
2024-02-02 07:21:27 -0800

    Duplicate RemoteFeedLoader as RemoteImageCommentsLoader

---
+++ EssentialFeedAPI/Image Comments/ImageCommentsMapper.swift
@@ -0,0 +1,22 @@
+//
+//  ImageCommentsMapper.swift
+//  EssentialFeedAPI
+//
+//  Created by Cleo Howard on 2/2/24.
+//
+
+import Foundation
+
+final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+        
+        return root.items
+    }
+}
