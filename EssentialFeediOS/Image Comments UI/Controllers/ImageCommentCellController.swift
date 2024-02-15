baf12b32619d8e847c2a3a677ba309ac1ac50bcd
lolearningcode <lolearningcode@gmail.com>
2024-02-15 09:35:39 -0800

    Add default implementation for optional methods

--- EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -22,12 +22,4 @@ public class ImageCommentCellController: CellController {
         
         return cell
     }
-    
-    public func preload() {
-        
-    }
-    
-    public func cancelLoad() {
-        
-    }
 }
